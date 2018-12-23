4cb367bfa8abd884751f4dd3f62c66c05103f004
xiandhcc/chenhongfei <chrunlee@foxmail.com>
2018-12-23 18:54:54 +0800

    V1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
 # sqlquery
-sql query tools for mysql or others? 
+sql query sqlquerys for mysql or others? 
+
+主要是提供一些简单数据库表的查询、更新、删除，如果是比较复杂的语句，还是可以直接写入sql语句来查询。
+
+//目前主要是自己使用，经常做一些小站，不需要太复杂的SQL，但是要写好多好多的lib包，才能开始数据库查询，还是很麻烦。
+
+# 使用说明
+
+- 安装
+```
+npm install sqlquery 
+```
+
+- 引入
+```
+let sqlquery = require('sqlquery');
+//配置链接数据库
+sqlquery({
+	host : '127.0.0.1',
+	port : '3306',
+	user : 'root',
+	password : 'root',
+	database : 'test'
+});
+```
+
+- 查询所有记录
+```
+sqlquery.search('sys_user').list()
+.then(rs=>{
+	console.log(rs);//获得所有记录
+})
+.catch(err=>{
+	console.log(err);
+})
+```
+
+API 说明:
+
+	1.sqlquery.search(tableName) 添加主表
+	2.sqlquery.search(tableName).where(whereObject) 添加查询条件
+	3.sqlquery.search(tableName).where(whereObject).or(orObject) 添加or查询条件
+	4.sqlquery.search(tableName).where(whereObject).or(orObject).order(orderObject) 添加排序
+	5.sqlquery.search(tableName).join(otherTable) 添加关联查询
+	6.sqlquery.search(tableName).limit(1,3) 添加条数限制
+	7.sqlquery.search(tableName).list() 查询记录
+	8.sqlquery.search(tableName).delete() 删除记录
+	9.sqlquery.search(tableName).update(data) 更新记录
+	10.sqlquery.search(tableName).insert(data) 插入记录
+	11.sqlquery.query({sql : sql,params : [p1]}) 执行复杂sql语句
+
+	相关的配置参数和格式下面继续说明
+
+- where(whereObj)
+
+用于添加限制条件，where中的对象，目前只能用一个，不能多次使用，否则会覆盖。
+```
+sqlquery.search('sys_user').where({
+	name : 'test',
+	title : {
+		alias : 't1',//如果有关联表，可以写关联表的别名
+		value : '%test%',
+		like : true // 增加like 为like语句
+	}
+})
+```
+
+在where中所有的条件之间的关系都是 `and` 关系。
+
+- join(tableName,options)
+
+用于添加关联表，加入后可以关联查询（与where中的alias对应）
+
+```
+sqlquery.search('sys_user').join('sys_article',{
+	alias : 't1',//alias可以不写，按照顺序编名
+	join : [{main : 'articleid',foreign : 'id'}]//在join中可以增加多个on条件，main为主表的字段，foreign为关联表的字段
+})
+```
+
+- or(orObject)
+
+用于or关系的限制条件 ，与where之间使用or关系，在or中自己对象内为and的关系，多个orObject的关系为or,如下：
+
+```
+sqlquery.search('sys_user').where({id : '1'}).or({name : 'test',desc : 'abc'}).or({title : {value : '%test%',like : true}})
+
+//生成的语句为下
+-- select  22476g.* from sys_user as 22476g where 22476g.id = ?    or (  (  22476g.name = ? and 22476g.desc = ?  ) or (  22476g.title like ?  )  )
+```
+
+- order(orderObj)
+
+用于添加排序字段，可以支持关联表字段排序，如下：
+```
+sqlquery.search('sys_search').order({column : 'id',order : 'asc'}).order([{column : 'name',alias : 't1',order : 'desc'},{column : 'ctime',order : 'desc'}])
+```
+
+- limit(start,rows)
+
+用于限制条数语句
+```
+sqlquery.search('sys_search').limit(1,10).list() 
+```
+
+- 查询类型：增删改查
+
+insert : 新增
+delete : 删除
+update : 更新
+list : 查询。
+
+其中，更新和新增都需要传递数据对象。
+```
+sqlquery('sys_user').update({
+	name : 'test'
+})
+
+sqlquery('sys_user').insert({
+	name : 'test'
+})
+```
+
+这四个函数都是封装的`promise`，可以直接以promise形式返回值。除了这四个函数之外，其余的都是链式调用。
+
+# 说明
+目前正在实验阶段，随时会修改。
+
+
+# License
+MIT LICENSE
---
+++ index.js
@@ -0,0 +1,254 @@
+/***
+ * sql query tool
+ * @author chrunlee
+ ***/
+
+let query = require('simple-mysql-query');
+
+//test --> 
+query({
+	host : '127.0.0.1',
+	port : '3306',
+	user : 'root',
+	password : 'root',
+	database : 'items'
+});
+
+let tool = {
+	query : query,
+	cache : [],//存放scheme
+	scheme : {},
+	/***
+	 * 提供表名，查询表中的字段数据
+	 ***/
+	desc (table){
+		return query({sql :`desc ${table}`,params : []})
+		.then(rs=>{
+			return rs[0];
+		});
+	},
+	_getScheme (){
+		return tool.scheme;
+	},
+	//确定表名
+	/***
+	 * tool.search('sys_user').list() //增加主表
+	 *
+	 ***/
+	search (table){
+		// let scheme = tool.getScheme();
+		let scheme = tool._getScheme();
+		scheme.table = table;
+		return tool;
+	},
+	//and 的查询条件
+	/*******
+	 * tool.search('sys_user').where({
+	 * 		id : '111',
+	 *		name : {
+	 *			table : 't1',value : 'test' // table : join 表的别名
+	 *		}	
+	 * }).list()
+	 ******/
+	where(obj){
+		let scheme = tool._getScheme();
+		scheme.where = obj;
+		return tool;
+	},
+	//链接其他表
+	/*****
+	 *
+	 * tool.search('sys_user').join('sys_article',{
+	 *  	alias : 't1',
+  	 *		join : [{main :'id',foreign : 'userid'}] //main 主表字段名，foreign : join表字段名
+	 * }).list()
+	 *
+	 *****/
+	join(table,options){
+		let scheme = tool._getScheme();
+		scheme.join = (scheme.join||[]).concat([{table : table,options : options||{}}]);
+		return tool;
+	},
+	//表排序
+	/******
+	 * tool.search('sys_user').order({
+	 *		column : 'createtime',order : 'desc',alias : 't1'//alias 表别名，不写为主表
+	 * }).list()
+	 *
+	 ***/
+	order( orderArr ){
+		let scheme = tool._getScheme();
+		orderArr = (typeof orderArr == 'object' && orderArr.length>=0) ? orderArr : [orderArr];
+		scheme.order = (scheme.order||[]).concat(orderArr);
+		return tool;
+	},
+	//查询条件or
+	/***** 
+	 * tool.search('sys_user').where({name : 'test'}).or({title : 'test'}).list();
+	 * //or 中不同object之间的关系为 or 与 where的关系也为or,但是同一个object中的关系为and ，与like正好相反
+	 *
+	 *****/
+	or( orobj ){
+		let scheme = tool._getScheme();
+		scheme.or = (scheme.or||[]).concat(orobj);
+		return tool;
+	},
+	//限制数据
+	/*****
+	 * tool.search('sys_user').limit(1,3).list();//limit(start,rows)
+	 *
+	 ****/
+	limit (start,rows){
+		let scheme = tool._getScheme();
+		scheme.limit = {start : start,rows : rows};
+		return tool;
+	},
+	//结束调用，必须调用list ,update ,delete ,insert 的一个
+	list (){
+		return query(tool._createSql('select',tool._getScheme())).then(rs=>{
+			return rs[0];
+		})
+	},
+	//结束调用，更新
+	update( data ){//更新data中的数据
+		return query(tool._createSql('update',tool._getScheme(),data)).then(rs=>{
+			return rs[0];
+		});
+	},
+	delete( ){//更新data中的数据
+		return query(tool._createSql('delete',tool._getScheme())).then(rs=>{
+			return rs[0];
+		});
+	},
+	//插入语句，没有where等条件
+	insert( data ){//更新data中的数据
+		return query(tool._createSql('insert',tool._getScheme(),data)).then(rs=>{
+			return rs[0];
+		});
+	},
+	//根据scheme生成sql
+	_createSql (type,schemeObj,valueObj){
+		let scheme = schemeObj || tool._getScheme();
+		let sql = `${type} `;
+		let params = [];
+		//判断有没有join,如果有join,则给table命名 t1或给一个特定的id
+		let tableAlias = ((+new Date())+'g').substr(8);
+
+		/**
+		 * select 查询相关语句
+		 **/
+
+
+		if(type == 'select'){
+			if(scheme.join && scheme.join.length > 0){
+				//把join的table加入
+				sql += ` ${tableAlias}.*`;
+				sql += scheme.join.reduce((_,a,i)=>{
+					return _ +`,${a.options.alias || 't'+i}.*`;
+				},'');
+			}else{
+				sql += ` ${tableAlias}.* from ${scheme.table} as ${tableAlias}`;
+			}
+			//继续left join
+			if(scheme.join && scheme.join.length > 0){
+				sql += `  from ${scheme.table} as ${tableAlias}`;
+				sql += scheme.join.reduce((_,a,i)=>{
+					let alias = a.options.alias || 't'+i;//join 表的别名
+					let joinStr = (a.options.join||[]).reduce((__,b,j)=>{
+						return __ + `${j == 0 ? '' : 'and'} ${tableAlias}.${b.main} = ${alias}.${b.foreign} `;
+					},'') || ` ${tableAlias}.id = ${alias}.id `;
+					return _ + ` left join ${a.table} as ${alias} on ${joinStr}`;
+				},'');
+			}
+
+			/***
+			 * update 更新相关语句
+ 			 **/
+		}else if(type == 'update'){
+			sql += ` ${scheme.table} as ${tableAlias} set `;
+			console.log(valueObj);
+			for(let key in valueObj){
+				sql += ` ${key}=?,`;
+				params.push(valueObj[key]||'');
+			}
+			sql = sql.substr(0,sql.length-1);
+
+			/***
+			 * delete 相关的语句
+			 *
+			 ***/
+		}else if(type == 'delete'){
+			sql += ` ${tableAlias} from ${scheme.table} as ${tableAlias}  `;
+
+			/***
+			 * insert into 语句插入
+			 *
+			 ***/
+		}else if(type == 'insert'){
+			sql += ` into ${scheme.table} ( `;
+			//遍历data.
+			let tempStr = ` values (`;
+			for(var key in valueObj){
+				sql += `${key},`;
+				tempStr += `?,`;
+				params.push(valueObj[key]);
+			}
+			sql = sql.substr(0,sql.length -1);
+			tempStr = tempStr.substr(0,tempStr.length -1);
+			tempStr += ') ';
+			sql += ') '+tempStr;
+			//insert 语句nowhere
+			return {sql : sql,params : params};
+		}
+
+		
+		//where 语句
+		if(scheme.where){
+			//默认都是主表的字段，否则根据table -value 来定值
+			sql += ` where`;
+			for(let name in scheme.where){
+				let nameOpt = scheme.where[name];
+				let whereAlias = typeof nameOpt == 'object' ? (nameOpt.table || tableAlias) : tableAlias;
+				let whereValue = typeof nameOpt == 'object' ? (nameOpt.value || '') : nameOpt;
+				let tempOperate = typeof nameOpt == 'object' ? (nameOpt.like ? 'like' : '=') : '='
+				sql += ` ${whereAlias}.${name} ${tempOperate} ?  and`;
+				params.push(whereValue);
+			}
+			sql = sql.substr(0,sql.length -3);
+		}
+		//or 与where 类似，不同的是or里面都是用的and
+		if(scheme.or && scheme.or.length > 0){	
+			sql += ` ${sql.indexOf('where') >-1 || sql.indexOf('and') >-1 ? ' or ' : ' where '}`;
+			let tempSql = scheme.or.reduce((_,a,i)=>{
+				let tempStr = '';
+				for(let key in a){
+					let tempAlias = typeof a[key] == 'object' ? (a[key].alias||tableAlias) : tableAlias;
+					let tempValue = typeof a[key] == 'object' ? a[key].value : a[key];
+					let tempOperate = typeof a[key] == 'object' ? (a[key].like ? 'like' : '=') : '='
+					tempStr += ` ${tempAlias}.${key} ${tempOperate} ? and`;
+					params.push(tempValue);
+				}
+				tempStr = tempStr.substr(0,tempStr.length-3);
+				return _ + ` ( ${tempStr} ) or`;
+			},'');
+			tempSql = `( ${tempSql.substr(0,tempSql.length -2)} )`;
+			sql += tempSql;
+		}
+		
+		//多条order记录，按照顺序来
+		if(scheme.order && scheme.order.length > 0){
+			sql += ` order by `;
+			sql = scheme.order.reduce((_,a,i)=>{
+				return sql + ` ${a.alias||tableAlias}.${a.column} ${a.order} ,`;
+			},sql);
+			sql = sql.substr(0,sql.length -1);
+		}
+		if(scheme.limit){
+			sql += ` limit ${scheme.limit.start},${scheme.limit.rows}`;
+		}
+		console.log(sql);
+		return {sql : sql,params : params};
+	}
+};
+// module.exports = tool;
+tool.search('sys_user').where({id : '1'}).or({name : 'test',desc : 'abc'}).or({title : {value : '%test%',like : true}}).list();
